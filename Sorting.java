a3a0c670768bbd9749e1badfc4352c3737451e4c
katejeongg <suhinjeong@gmail.com>
2024-09-09 00:07:25 -0400

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -3,40 +3,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 
-/**
- * Your implementation of various sorting algorithms.
- *
- * @author KATE JEONG
- * @version 1.0
- * @userid kjeong40
- * @GTID 903886263
- *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
- *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
- */
 public class Sorting {
 
-    /**
-     * Implement selection sort.
-     *
-     * It should be:
-     * in-place
-     * unstable
-     * not adaptive
-     *
-     * Have a worst case running time of:
-     * O(n^2)
-     *
-     * And a best case running time of:
-     * O(n^2)
-     *
-     * @param <T>        data type to sort
-     * @param arr        the array that must be sorted after the method runs
-     * @param comparator the Comparator used to compare the data in arr
-     * @throws IllegalArgumentException if the array or comparator is
-     *                                            null
-     */
     public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
         if (arr == null || comparator == null) {
             throw new IllegalArgumentException("Array or comparator is null.");
@@ -62,26 +30,6 @@ public class Sorting {
         }
     }
 
-    /**
-     * Implement insertion sort.
-     *
-     * It should be:
-     * in-place
-     * stable
-     * adaptive
-     *
-     * Have a worst case running time of:
-     * O(n^2)
-     *
-     * And a best case running time of:
-     * O(n)
-     *
-     * @param <T>        data type to sort
-     * @param arr        the array that must be sorted after the method runs
-     * @param comparator the Comparator used to compare the data in arr
-     * @throws IllegalArgumentException if the array or comparator is
-     *                                            null
-     */
     public static <T> void insertionSort(T[] arr, Comparator<T> comparator) {
         if (arr == null || comparator == null) {
             throw new IllegalArgumentException("Array or comparator is null.");
@@ -103,29 +51,6 @@ public class Sorting {
         }
     }
 
-    /**
-     * Implement bubble sort.
-     *
-     * It should be:
-     * in-place
-     * stable
-     * adaptive
-     *
-     * Have a worst case running time of:
-     * O(n^2)
-     *
-     * And a best case running time of:
-     * O(n)
-     *
-     * NOTE: See pdf for last swapped optimization for bubble sort. You
-     * MUST implement bubble sort with this optimization
-     *
-     * @param <T>        data type to sort
-     * @param arr        the array that must be sorted after the method runs
-     * @param comparator the Comparator used to compare the data in arr
-     * @throws IllegalArgumentException if the array or comparator is
-     *                                            null
-     */
     public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
         if (arr == null || comparator == null) {
             throw new IllegalArgumentException("Array or comparator is null.");
@@ -153,35 +78,6 @@ public class Sorting {
         }
     }
 
-    /**
-     * Implement merge sort.
-     *
-     * It should be:
-     * out-of-place
-     * stable
-     * not adaptive
-     *
-     * Have a worst case running time of:
-     * O(n log n)
-     *
-     * And a best case running time of:
-     * O(n log n)
-     *
-     * You can create more arrays to run merge sort, but at the end, everything
-     * should be merged back into the original T[] which was passed in.
-     *
-     * When splitting the array, if there is an odd number of elements, put the
-     * extra data on the right side.
-     *
-     * Hint: If two data are equal when merging, think about which subarray
-     * you should pull from first
-     *
-     * @param <T>        data type to sort
-     * @param arr        the array to be sorted
-     * @param comparator the Comparator used to compare the data in arr
-     * @throws IllegalArgumentException if the array or comparator is
-     *                                            null
-     */
     public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
         if (arr == null || comparator == null) {
             throw new IllegalArgumentException("Array or comparator is null.");
@@ -228,47 +124,6 @@ public class Sorting {
         }
     }
 
-    /**
-     * Implement LSD (least significant digit) radix sort.
-     *
-     * Make sure you code the algorithm as you have been taught it in class.
-     * There are several versions of this algorithm and you may not get full
-     * credit if you do not implement the one we have taught you!
-     *
-     * Remember you CANNOT convert the ints to strings at any point in your
-     * code! Doing so may result in a 0 for the implementation.
-     *
-     * It should be:
-     * out-of-place
-     * stable
-     * not adaptive
-     *
-     * Have a worst case running time of:
-     * O(kn)
-     *
-     * And a best case running time of:
-     * O(kn)
-     *
-     * You are allowed to make an initial O(n) passthrough of the array to
-     * determine the number of iterations you need.
-     *
-     * At no point should you find yourself needing a way to exponentiate a
-     * number; any such method would be non-O(1). Think about how how you can
-     * get each power of BASE naturally and efficiently as the algorithm
-     * progresses through each digit.
-     *
-     * Refer to the PDF for more information on LSD Radix Sort.
-     *
-     * You may use ArrayList or LinkedList if you wish, but it may only be
-     * used inside radix sort and any radix sort helpers. Do NOT use these
-     * classes with other sorts. However, be sure the List implementation you
-     * choose allows for stability while being as efficient as possible.
-     *
-     * Do NOT use anything from the Math class except Math.abs().
-     *
-     * @param arr the array to be sorted
-     * @throws IllegalArgumentException if the array is null
-     */
     public static void lsdRadixSort(int[] arr) {
         if (arr == null) {
             throw new IllegalArgumentException("Array is null.");
@@ -318,35 +173,6 @@ public class Sorting {
         }
     }
 
-    /**
-     * Implement heap sort.
-     *
-     * It should be:
-     * out-of-place
-     * unstable
-     * not adaptive
-     *
-     * Have a worst case running time of:
-     * O(n log n)
-     *
-     * And a best case running time of:
-     * O(n log n)
-     *
-     * Use java.util.PriorityQueue as the heap. Note that in this
-     * PriorityQueue implementation, elements are removed from smallest
-     * element to largest element.
-     *
-     * Initialize the PriorityQueue using its build heap constructor (look at
-     * the different constructors of java.util.PriorityQueue).
-     *
-     * Return an int array with a capacity equal to the size of the list. The
-     * returned array should have the elements in the list in sorted order.
-     *
-     * @param data the data to sort
-     * @return the array with length equal to the size of the input list that
-     * holds the elements from the list is sorted order
-     * @throws IllegalArgumentException if the data is null
-     */
     public static int[] heapSort(List<Integer> data) {
         // create heap from array w buildheap (automatic bc priorityqueue)
         // for i = 0 to arr.length, pop from heap into array at i
